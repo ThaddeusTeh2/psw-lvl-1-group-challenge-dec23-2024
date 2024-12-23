fc34d90362c12db9bec91c089ef7a63ee4c8c81a
Thaddeus Teh :D <thaddeusteh2@gmail.com>
2024-12-23 23:44:07 +0800

    Co-authored-by: Keshen <keshster98@users.noreply.github.com> Co-authored-by: BrianTangLeZhang <BrianTangLeZhang@users.noreply.github.com>

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -47,54 +47,79 @@ function Home() {
           <AddIcon /> Add New Task
         </Button>
       </Stack>
-      {priorities.map(it => ())
+      {tasks.length !== 0 ? (
+        priorities.map((it) => (
+          <>
+            <Typography variant="h6">Priority: {it}</Typography>
+            <Grid
+              container
+              spacing={2}
+              sx={{ marginBottom: 10 }}
+              display={"flex"}
+              justifyContent={"center"}
+            >
+              {tasks
+                .filter((task) => task.priority === it)
+                .map((task) => (
+                  <Grid size={12} key={task.id}>
+                    <Card sx={{ minWidth: 275 }}>
+                      <Box
+                        display="flex"
+                        justifyContent="space-between"
+                        alignItems="center"
+                        padding={2}
+                      >
+                        <Typography variant="h6" component="div">
+                          {task.name}
+                        </Typography>
 
-      }
-      <Grid container spacing={2} display={"flex"} justifyContent={"center"}>
-        {tasks.map((task) => (
-          <Grid size={12} key={task.id}>
-            <Card sx={{ minWidth: 275 }}>
-              <Box
-                display="flex"
-                justifyContent="space-between"
-                alignItems="center"
-                padding={2}
-              >
-                <Typography variant="h6" component="div">
-                  {task.name}
-                </Typography>
+                        <CardContent>
+                          <Box display="flex" gap={1}>
+                            <Chip
+                              label={`Priority: ${task.priority}`}
+                              color={
+                                task.priority === "High" ? "error" : "primary"
+                              }
+                              variant="outlined"
+                              size="small"
+                            />
+                            <Chip
+                              label={`Due: ${task.dueDate}`}
+                              color="error"
+                              variant="outlined"
+                              size="small"
+                            />
+                          </Box>
+                        </CardContent>
 
-                <CardContent>
-                  <Box display="flex" gap={1}>
-                    <Chip
-                      label={`Priority: ${task.priority}`}
-                      color={task.priority === "High" ? "error" : "primary"}
-                      variant="outlined"
-                      size="small"
-                    />
-                    <Chip
-                      label={`Due: ${task.dueDate}`}
-                      color="error"
-                      variant="outlined"
-                      size="small"
-                    />
-                  </Box>
-                </CardContent>
-
-                <CardActions>
-                  <Button
-                    variant="outlined"
-                    color="error"
-                    onClick={() => handleTaskDelete(task.id)}
-                  >
-                    Delete
-                  </Button>
-                </CardActions>
-              </Box>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
+                        <CardActions>
+                          <Button
+                            variant="outlined"
+                            color="error"
+                            onClick={() => handleTaskDelete(task.id)}
+                          >
+                            Delete
+                          </Button>
+                        </CardActions>
+                      </Box>
+                    </Card>
+                  </Grid>
+                ))}
+            </Grid>
+          </>
+        ))
+      ) : (
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            paddingY: 10,
+          }}
+        >
+          <Typography variant="h6">No task added yet</Typography>
+        </Box>
+      )}
     </Container>
   );
 }
